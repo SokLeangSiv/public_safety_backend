471f413aed5586571fd12fd3d2c3259694313ebd
leangsiv <leangsiv7777@gmail.com>
2024-07-07 17:27:06 +0700

    done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,15 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        DB::table('admin')->insert([
+            'name'=> 'admin',
+            'email'=> 'admin@gmail.com',
+            'password'=> Hash::make('12345678'),
         ]);
     }
 }
